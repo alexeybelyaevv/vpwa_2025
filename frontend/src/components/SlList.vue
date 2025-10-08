4387926c9b5f07e001bf29af8e9875bc74ab0a1d
Sofia <sofapokl@gmail.com>
2025-10-08 15:06:47 +0300

    feat: implemented basic panels, workspace page

---
+++ frontend/src/components/SlList.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="sl-list">
+        <h3>{{ title }}</h3>
+        <ul>
+            <li v-for="item in list" :key="item.id">
+                {{ item.title }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { Chat } from 'src/types'
+defineProps<{
+    list: Chat[],
+    title: string
+}>()
+</script>
+<style scoped>
+.sl-list {
+  width: 220px;
+  background-color: #1e1f22;
+  border-right: 1px solid #2e2f33;
+  padding: 15px;
+  h3 {
+    margin-bottom: 10px;
+    color: #bbb;
+    font-size: 14px;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+    li {
+      padding: 6px 0;
+      color: #ccc;
+      cursor: pointer;
+      &:hover {
+        color: #fff;
+      }
+    }
+  }
+}
+</style>
---
+++ frontend/src/components/SlSidebar.vue
@@ -0,0 +1,17 @@
+<template>
+    <aside class="sl-sidebar">
+        <p>User Panel</p>
+    </aside>
+</template>
+<script setup lang="ts">
+</script>
+<style scoped>
+.sl-sidebar {
+  width: 70px;
+  background-color: #350d36;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-right: 1px solid #532753;
+}
+</style>
